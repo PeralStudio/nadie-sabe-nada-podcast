3b6aa707df7d11266ce6e2899d7116869b4ae2e4
Alberto <66646508+PeralStudio@users.noreply.github.com>
2024-10-24 17:35:04 +0200

    ✨ feat : Added redux files

--- src/store/slices/playerSlice.js
+++ src/store/slices/playerSlice.js
@@ -1,35 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const closePersistentPlayerAsync = createAsyncThunk(
+    "player/closePersistentPlayerAsync",
+    (_, { dispatch }) => {
+        dispatch(playerSlice.actions.closePlayer());
+
+        setTimeout(() => {
+            dispatch(playerSlice.actions.clearCurrentPodcast());
+        }, 800);
+    }
+);
 
 const playerSlice = createSlice({
-  name: 'player',
-  initialState: {
-    currentPodcast: null,
-    isPlaying: false,
-    showPlayer: false,
-  },
-  reducers: {
-    setCurrentPodcast: (state, action) => {
-      state.currentPodcast = action.payload;
+    name: "player",
+    initialState: {
+        currentPodcast: null,
+        isPlaying: false,
+        showPlayer: false
     },
-    togglePlay: (state, action) => {
-      state.isPlaying = action.payload !== undefined ? action.payload : !state.isPlaying;
-    },
-    setShowPlayer: (state, action) => {
-      state.showPlayer = action.payload;
-    },
-    closePersistentPlayer: (state) => {
-      state.currentPodcast = null;
-      state.isPlaying = false;
-      state.showPlayer = false;
-    },
-  },
+    reducers: {
+        setCurrentPodcast: (state, action) => {
+            state.currentPodcast = action.payload;
+        },
+        togglePlay: (state, action) => {
+            state.isPlaying = action.payload !== undefined ? action.payload : !state.isPlaying;
+        },
+        setShowPlayer: (state, action) => {
+            state.showPlayer = action.payload;
+        },
+        closePlayer: (state) => {
+            state.isPlaying = false;
+            state.showPlayer = false;
+        },
+        clearCurrentPodcast: (state) => {
+            state.currentPodcast = null;
+        }
+    }
 });
 
-export const { 
-  setCurrentPodcast, 
-  togglePlay, 
-  setShowPlayer, 
-  closePersistentPlayer 
-} = playerSlice.actions;
+export const { setCurrentPodcast, togglePlay, setShowPlayer, closePlayer, clearCurrentPodcast } =
+    playerSlice.actions;
 
 export default playerSlice.reducer;
